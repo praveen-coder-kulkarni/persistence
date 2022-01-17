4c14415b33d59ab3855ff4ed30acb76dece41ab4
Praveen Kumar K <praveen.code.kulkarni@gmail.com>
2022-01-17 22:08:30 +0530

    Created this codebase to demonstrate interacting with SQL Database from Spring application: - Added the dependencies: Spring Data JPA & H2 while creating the project using Spring Initializer. - Set the logging.level.sql to debug. - Added schema.sql & data.sql files run the DDL & DML files as soon as project is run.

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO officers(rank, first_name, last_name) values ('CAPTAIN', 'James', 'Kirk');
+INSERT INTO officers(rank, first_name, last_name) values ('CAPTAIN', 'Jean-Luc', 'Picard');
+INSERT INTO officers(rank, first_name, last_name) values ('CAPTAIN', 'Benjamin', 'Sisko');
+INSERT INTO officers(rank, first_name, last_name) values ('CAPTAIN', 'Kathryn', 'Janeway');
+INSERT INTO officers(rank, first_name, last_name) values ('CAPTAIN', 'Jonathan', 'Archer');
---
+++ src/test/java/com/oreilly/persistence/PersistenceApplicationTests.java
@@ -0,0 +1,14 @@
+package com.oreilly.persistence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersistenceApplicationTests {
+
+   @Test
+   void contextLoads() {
+
+   }
+
+}
